1e4af3b08f3b23e97b320bdaf77e94c94622bf74
Simon Farshid <Yonom@users.noreply.github.com>
2023-11-10 23:10:38 +0200

    Clean up camera view

--- app/src/CameraView.tsx
+++ app/src/CameraView.tsx
@@ -4,12 +4,66 @@ import MlkitOdt, {
   ObjectDetectionResult,
   ObjectDetectorMode,
 } from "react-native-mlkit-odt";
-import { Camera, useCameraDevice } from "react-native-vision-camera";
+import {
+  Camera,
+  PhotoFile,
+  useCameraDevice,
+  useCameraFormat,
+} from "react-native-vision-camera";
+
+type BoundingBoxResult = {
+  top: number;
+  left: number;
+  width: number;
+  height: number;
+};
+
+const getObjectDetectionResult = (photo: PhotoFile) => {
+  return MlkitOdt.detectFromUri("file:///" + photo.path, {
+    detectorMode: ObjectDetectorMode.SINGLE_IMAGE,
+    shouldEnableClassification: false,
+    shouldEnableMultipleObjects: false,
+  });
+};
+
+const getBoundingBox = (
+  photo: PhotoFile,
+  res: ObjectDetectionResult | undefined
+) => {
+  if (!res) return;
+  const bounding = res.bounding;
+
+  const { width: windowWidth, height: windowHeight } = Dimensions.get("window");
+  const { width: photoWidth, height: photoHeight } = photo;
+  const normalized = {
+    top: bounding.originX / photoWidth,
+    leftEdgeFromRight: bounding.originY / photoHeight,
+    height: bounding.width / photoWidth,
+    width: bounding.height / photoHeight,
+  };
+  return {
+    top: normalized.top * windowHeight,
+    left:
+      windowWidth -
+      normalized.width * windowWidth -
+      normalized.leftEdgeFromRight * windowWidth,
+    height: normalized.height * windowHeight,
+    width: normalized.width * windowWidth,
+  };
+};
 
 export const CameraView = () => {
   const device = useCameraDevice("back");
+  const format = useCameraFormat(device, [
+    {
+      photoResolution: {
+        width: 1920,
+        height: 1080,
+      },
+    },
+  ]);
   const ref = useRef<Camera>(null);
-  const [boundingBoxes, setBoundingBoxes] = useState<ObjectDetectionResult[]>();
+  const [boundingBox, setBoundingBox] = useState<BoundingBoxResult>();
 
   useEffect(() => {
     let ended = false;
@@ -21,15 +75,12 @@ export const CameraView = () => {
           qualityPrioritization: "speed",
           enableShutterSound: false,
         });
-        console.log(Dimensions.get("window"), photo);
-        const result = await MlkitOdt.detectFromUri("file:///" + photo.path, {
-          detectorMode: ObjectDetectorMode.SINGLE_IMAGE,
-          shouldEnableClassification: false,
-          shouldEnableMultipleObjects: false,
-        });
-        console.log(result);
-        setBoundingBoxes(result);
+        const result = await getObjectDetectionResult(photo);
+        const boundingBox = getBoundingBox(photo, result[0]);
+        setBoundingBox(boundingBox);
+
         // await FileSystem.deleteAsync(photo.path);
+
         runLoop();
       }, 1000);
     };
@@ -39,35 +90,39 @@ export const CameraView = () => {
     };
   }, []);
 
-  if (device == null) return null;
   return (
     <>
       <Camera
         style={StyleSheet.absoluteFill}
         device={device}
+        format={format}
         isActive={true}
         ref={ref}
         photo={true}
+        orientation="portrait"
       />
-      {boundingBoxes?.map((box, index) => (
+      {boundingBox && (
         <View
-          key={index}
           style={{
-            top:
-              (box.bounding.originX / 1920) * Dimensions.get("window").height,
-            left:
-              Dimensions.get("window").width -
-              (box.bounding.height / 1080) * Dimensions.get("window").width -
-              (box.bounding.originY / 1080) * Dimensions.get("window").width,
-            height:
-              (box.bounding.width / 1920) * Dimensions.get("window").height,
-            width:
-              (box.bounding.height / 1080) * Dimensions.get("window").width,
+            position: "absolute",
+            top: boundingBox.top,
+            left: boundingBox.left,
+            width: boundingBox.width,
+            height: boundingBox.height,
             borderWidth: 2,
             borderColor: "red",
           }}
         />
-      ))}
+      )}
+      {/* {boundingBoxes?.map((box, index) => (
+        <View
+          key={index}
+          style={{
+            borderWidth: 2,
+            borderColor: "red",
+          }}
+        />
+      ))} */}
     </>
   );
 };
