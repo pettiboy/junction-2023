1e13a88bb795dc9de509fc3be2ff88a2bafc88c7
Simon Farshid <Yonom@users.noreply.github.com>
2023-11-10 20:31:18 +0200

    Add react-native-vision-camera

--- app/App.js
+++ app/App.js
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from "react-native";
 
 export default function App() {
+  const device = useCameraDevice("back");
+
+  if (device == null) return <NoCameraErrorView />;
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Camera style={StyleSheet.absoluteFill} device={device} isActive={true} />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
