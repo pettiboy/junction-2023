b2fe4702502db1abc85396f1425cd4501912ae90
Simon Farshid <Yonom@users.noreply.github.com>
2023-11-10 20:27:29 +0200

    Initialize app

---
+++ app/babel.config.js
@@ -0,0 +1,14 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: [
+      "babel-preset-expo",
+      [
+        "react-native-reanimated/plugin",
+        {
+          globals: ["__labelImage"],
+        },
+      ],
+    ],
+  };
+};
