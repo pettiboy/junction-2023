467fd475a5ef2db645b8cca0cea8c9d9164f3c85
jaymalave <jaym.github@gmail.com>
2023-11-11 12:54:21 +0200

    ft(backend): gpt response parsed

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,8 +1,8 @@
 from app import app
-
-# from openai import OpenAI
+from openai import OpenAI
 import requests
 from flask import request
+import json
 import os
 
 # import sys
@@ -54,3 +54,45 @@ def generate_response():
     print(response.json())
 
     return response.json()
+
+
+@app.route("/generate-response-2", methods=["POST"])
+def generate_typed_response():
+    data = request.get_json()
+    client = OpenAI(api_key=gpt_api_key)
+
+    schema = {
+        "type": "object",
+        "properties": {
+            "item": {"type": "string"},
+            "recyclability_score": {"type": "string"},
+            "how_to_recycle": {"type": "string"},
+        },
+    }
+
+    response = client.chat.completions.create(
+        model="gpt-4-vision-preview",
+        # response_format={"type": "json_object"},
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": "Can you return only the JSON in the following format: item, recyclability_score, how_to_recycle? about the object in this image? Do not return any non-json text or numbering",
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {"url": f"data:image/jpeg;base64,{data['image']}"},
+                    },
+                ],
+            }
+        ],
+        # functions=[{"name": "get_typed_response", "parameters": schema}],
+        # function_call={"name": "get_typed_response"},
+        max_tokens=300,
+    )
+
+    print(response.choices[0].message.content)
+
+    return json.dumps(response.choices[0].message.content.replace("json", ""))
